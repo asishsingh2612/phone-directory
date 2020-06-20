01c5ed3f096fd5976888305250134287c56bdc28
asishsingh2612 <63581288+asishsingh2612@users.noreply.github.com>
2020-06-20 14:10:13 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+    <div className="header">
+      Phone Directory
+    </div>
+    <button>Add</button>
+    <div>
+      <span>Name</span><br/>
+      <span>Phone</span>
+    </div>
+    </div>
+  );
+}
+
+export default App;
